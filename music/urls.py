44b016ed666ac5de7d1eabf0912c369c8eaa432c
Ian Grosh <iangrosh@outlook.com>
2016-11-05 23:13:16 -0400

    lots of stuff

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "music"
+
 urlpatterns = [
-    url(r'^$', views.index,name="index"),
+    # /music/
+    url(r'^$', views.IndexView.as_view(),name="index"),
+    # /music/<albumID>/
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name="detail"),
+    # /music/album/add
+    url(r'album/add/$',views.AlbumCreate.as_view(),name="album-add")
+    # /music/album/<albumID>/update
+    #url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name="detail"),
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album, Song
 
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
 
-def index(request):
-    return HttpResponse("<h1> This is the music app home page </h1>")
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields= ['artist','album_title','genre','album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields= ['artist','album_title','genre','album_logo']
+
+class AlbumDelet(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
